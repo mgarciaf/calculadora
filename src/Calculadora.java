36ec178075cb71f606818394fecbe176dc7ed7e2
Ariel Alberto Escobar González <ariel.escobar.gonzalez@gmail.com>
2023-03-22 18:06:18 -0600

    Integrando las operaciones y el menu

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,7 +4,13 @@ public class Calculadora {
 
     public static void main(String[] args) {
         int opcion = 0;
+        int numero1 = 0;
+        int numero2 = 0;
+
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+
         Scanner scannerString = new Scanner(System.in);
         System.out.println("¿Qué operación desea realizar Suma(1) | Resta(2) | Multiplicación (3) | División (4) ?:");
 
@@ -12,18 +18,29 @@ public class Calculadora {
             mostrarMenu();
             opcion = scanner.nextInt();
 
+            System.out.println(" Digite el primer número");
+            numero1 = scanner1.nextInt();
+            System.out.println(" Digite el segundo número");
+            numero2 = scanner2.nextInt();
+
             switch (opcion) {
+
                 case 1:
-                    System.out.println("suma()");
+
+                    int suma = new Suma().operacion(numero1, numero2);
+                    System.out.println("Suma: " + suma);
                     break;
                 case 2:
-                    System.out.println("resta()");
+                    int resta = new Resta().operacion(numero1, numero2);
+                    System.out.println("Resta: " + resta);
                     break;
                 case 3:
-                    System.out.println("eliminar()");
+                    int mult = new Multiplicacion().operacion(numero1, numero2);
+                    System.out.println("Multiplicación: " + mult);
                     break;
                 case 4:
-                    System.out.println("division()");
+                    int division = new Division().operacion(numero1, numero2);
+                    System.out.println("División: " + division);
                     break;
                 default:
                     break;
